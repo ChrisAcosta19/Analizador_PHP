2f4e5d40abc96628048ac4800a85a4d97f6c1f7b
JeffErasLindao <jeras@espol.edu.ec>
2024-06-17 00:13:29 -0500

    Actualizacion_Jefferson_Token_creacionARCHIVOtxt

--- lexer.py
+++ lexer.py
@@ -1,77 +1,100 @@
+import os
+from datetime import datetime
 import ply.lex as lex
 
+# Obtener la ubicación del script actual
+script_dir = os.path.dirname(__file__)
+
+# Crear carpeta 'logs' en la misma ubicación que el script
+logs_dir = os.path.join(script_dir, 'logs')
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
 reserved = {
-    '__halt_compiler' : 'HALT_COMPILER',
-    'endif' : 'ENDIF',
-    'elseif' : 'ELSEIF',
-    'default' : 'DEFAULT',
-    'insteadof' : 'INSTEADOF',
-    'endforeach' : 'ENDFOREACH',
-    'declare' : 'DECLARE',
-    'array' : 'ARRAY',
-    'match' : 'MATCH',
-    'goto' : 'GOTO',
-    'fn' : 'FN',
-    'endfor' : 'ENDFOR',
-    'continue' : 'CONTINUE',
-    'case' : 'CASE',
-    'if' : 'IF',
-    'else' : 'ELSE',
-    'break' : 'BREAK',
-    'while' : 'WHILE',
-    'echo' : 'ECHO',
-    'for' : 'FOR',
-    'foreach' : 'FOREACH',
-    'function' : 'FUNCTION',
-    'return' : 'RETURN',
-    'class' : 'CLASS',
-    'interface' : 'INTERFACE',
-    'extends' : 'EXTENDS',
-    'implements' : 'IMPLEMENTS',
-    'public' : 'PUBLIC',
-    'private' : 'PRIVATE',
-    'protected' : 'PROTECTED',
-    'static' : 'STATIC',
-    'final' : 'FINAL',
-    'abstract' : 'ABSTRACT',
-    'new' : 'NEW',
-    'try' : 'TRY',
-    'catch' : 'CATCH',
-    'throw' : 'THROW',
-    'finally' : 'FINALLY',
-    'instanceof' : 'INSTANCEOF',
-    'namespace' : 'NAMESPACE',
-    'use' : 'USE',
-    'require' : 'REQUIRE',
-    'include' : 'INCLUDE',
-    'const' : 'CONST',
-    'and' : 'AND',
-    'or' : 'OR',
-    'xor' : 'XOR',
-    'print' : 'PRINT',
-    'echo' : 'ECHO',
-    'var' : 'VAR',
-    'global' : 'GLOBAL',
-    'isset' : 'ISSET',
-    'empty' : 'EMPTY',
-    'unset' : 'UNSET',
-    'die' : 'DIE',
-    'exit' : 'EXIT',
-    'eval' : 'EVAL',
-    'include_once' : 'INCLUDE_ONCE',
-    'require_once' : 'REQUIRE_ONCE',
-    'yield' : 'YIELD',
-    'yield from' : 'YIELD_FROM',
-    'as' : 'AS',
-    'endswitch' : 'ENDSWITCH',
-    'callable' : 'CALLABLE',
-    'do' : 'DO',
-    'enddeclare' : 'ENDDECLARE',
-    'endwhile' : 'ENDWHILE',
-    'list' : 'LIST',
-    'trait' : 'TRAIT',
-    'switch' : 'SWITCH',
-    'null' : 'NULL',
+    '__halt_compiler': 'HALT_COMPILER',
+    'endif': 'ENDIF',
+    'elseif': 'ELSEIF',
+    'default': 'DEFAULT',
+    'insteadof': 'INSTEADOF',
+    'endforeach': 'ENDFOREACH',
+    'declare': 'DECLARE',
+    'array': 'ARRAY',
+    'match': 'MATCH',
+    'goto': 'GOTO',
+    'fn': 'FN',
+    'endfor': 'ENDFOR',
+    'continue': 'CONTINUE',
+    'case': 'CASE',
+    'if': 'IF',
+    'else': 'ELSE',
+    'break': 'BREAK',
+    'while': 'WHILE',
+    'echo': 'ECHO',
+    'for': 'FOR',
+    'foreach': 'FOREACH',
+    'function': 'FUNCTION',
+    'return': 'RETURN',
+    'class': 'CLASS',
+    'interface': 'INTERFACE',
+    'extends': 'EXTENDS',
+    'implements': 'IMPLEMENTS',
+    'public': 'PUBLIC',
+    'private': 'PRIVATE',
+    'protected': 'PROTECTED',
+    'static': 'STATIC',
+    'final': 'FINAL',
+    'abstract': 'ABSTRACT',
+    'new': 'NEW',
+    'try': 'TRY',
+    'catch': 'CATCH',
+    'throw': 'THROW',
+    'finally': 'FINALLY',
+    'instanceof': 'INSTANCEOF',
+    'namespace': 'NAMESPACE',
+    'use': 'USE',
+    'require': 'REQUIRE',
+    'include': 'INCLUDE',
+    'const': 'CONST',
+    'and': 'AND',
+    'or': 'OR',
+    'xor': 'XOR',
+    'print': 'PRINT',
+    'echo': 'ECHO',
+    'var': 'VAR',
+    'global': 'GLOBAL',
+    'isset': 'ISSET',
+    'empty': 'EMPTY',
+    'unset': 'UNSET',
+    'die': 'DIE',
+    'exit': 'EXIT',
+    'eval': 'EVAL',
+    'include_once': 'INCLUDE_ONCE',
+    'require_once': 'REQUIRE_ONCE',
+    'yield': 'YIELD',
+    'yield from': 'YIELD_FROM',
+    'as': 'AS',
+    'endswitch': 'ENDSWITCH',
+    'callable': 'CALLABLE',
+    'do': 'DO',
+    'enddeclare': 'ENDDECLARE',
+    'endwhile': 'ENDWHILE',
+    'list': 'LIST',
+    'trait': 'TRAIT',
+    'switch': 'SWITCH',
+    'null': 'NULL',
+    'async': 'ASYNC',
+    'await': 'AWAIT',
+    'bool': 'BOOL',
+    'clone': 'CLONE',
+    'enum': 'ENUM',
+    'false': 'FALSE',
+    'iterable': 'ITERABLE',
+    'mixed': 'MIXED',
+    'object': 'OBJECT',
+    'self': 'SELF',
+    'string': 'STRING',
+    'true': 'TRUE',
+    'void': 'VOID',
 }
 
 # List of token names.   This is always required
@@ -89,8 +112,18 @@ tokens = [
     'POWER',
     'LPAREN',
     'RPAREN',
-
+    
     # Jefferson Eras
+    'LOGICAL_AND',
+    'LOGICAL_OR',
+    'LOGICAL_XOR',
+    'LESS_THAN',
+    'GREATER_THAN',
+    'LESS_EQUAL',
+    'GREATER_EQUAL',
+    'PHP_OPEN_TAG',
+    'PHP_CLOSE_TAG',
+
     # Peter Miranda
 ] + list(reserved.values())
 
@@ -104,20 +137,34 @@ t_POWER = r'\*\*'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
+
+def t_PHP_OPEN_TAG(t):
+    r'<\?php'
+    return t
+
+
+def t_PHP_CLOSE_TAG(t):
+    r'\?>'
+    return t
+
+
 def t_FLOAT(t):
     r'-?(\.\d+|\d+\.\d*)'
     t.value = float(t.value)
     return t
 
+
 def t_NAME(t):
     r'[a-zA-Z_]\w*'
-    t.type = reserved.get(t.value,'NAME')    # Check for reserved words
+    t.type = reserved.get(t.value, 'NAME')    # Check for reserved words
     return t
+
 
 def t_ID(t):
     r'\$[a-zA-Z_]\w*'
-    t.type = reserved.get(t.value[1:],'ID')    # Check for reserved words
+    t.type = reserved.get(t.value[1:], 'ID')    # Check for reserved words
     return t
+
 
 # A regular expression rule with some action code
 def t_INTEGER(t):
@@ -125,35 +172,106 @@ def t_INTEGER(t):
     t.value = int(t.value)
     return t
 
+
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
+# Jefferson Eras
+def t_TRUE(t):
+    r'true'
+    t.value = True
+    return t
+
+
+def t_FALSE(t):
+    r'false'
+    t.value = False
+    return t
+
+
+def t_STRING(t):
+    r'(\'[^\\\n]*(\\.[^\\\n]*)*\')|(\"[^\\\n]*(\\.[^\\\n]*)*\")'
+    return t
+
+
+def t_ARRAY(t):
+    r'array|\[\]'
+    return t
+
+
+def t_OBJECT(t):
+    r'object'
+    return t
+
+
+def t_ENUM(t):
+    r'enum'
+    return t
+
+
+def t_LOGICAL_AND(t):
+    r'&&|and'
+    return t
+
+
+def t_LOGICAL_OR(t):
+    r'\|\||or'
+    return t
+
+
+def t_LOGICAL_XOR(t):
+    r'xor'
+    return t
+
+
+# Comparison operators
+t_LESS_THAN = r'<'
+t_GREATER_THAN = r'>'
+t_LESS_EQUAL = r'<='
+t_GREATER_EQUAL = r'>='
+
 # A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
+t_ignore = ' \t\r\n'
+
 
 # Error handling rule
 def t_error(t):
-    print("Carácter inesperado '%s' en la linea %d, columna %d" %
-           (t.value[0], t.lexer.lineno, t.lexer.lexpos))
+    print("Carácter inesperado '%s' en la linea %d, columna %d" % 
+          (t.value[0], t.lexer.lineno, t.lexer.lexpos))
     t.lexer.skip(1)
+
 
 # Build the lexer
 lexer = lex.lex()
 
 # Test it out
 data = '''
-greet 20.34 3 % 4 ** 10
-  + -20 *2
+<?php 
+
+colocar algoritmo aqui
+
+?> 
 '''
 
-# Give the lexer some input
-lexer.input(data)
+# Generar nombre de archivo de log basado en la fecha y hora actual
+git_username = "JeffErasLindao"
+log_file_name = datetime.now().strftime(f'lexico-{git_username}-%d%m%Y-%Hh%M.txt')
 
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    print(tok)
+# Abrir archivo de log para escritura en la carpeta 'logs'
+with open(os.path.join(logs_dir, log_file_name), 'w') as log_file:
+    # Darle al lexer la entrada
+    lexer.input(data)
+
+    # Tokenizar
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break      # No hay más entrada
+        # Escribir token en archivo de log
+        log_file.write(f'{tok.type},{tok.value},{tok.lineno},{tok.lexpos}\n')
+
+# Mensaje de confirmación
+print(f'Archivo de log generado: {log_file_name} en la carpeta {logs_dir}.')
